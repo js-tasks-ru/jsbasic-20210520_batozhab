1a776c48dadf502603978f681316b82bf152e4e2
Batozhab <gorod3012@gmail.com>
2021-06-03 11:22:39 +0800

    Занятие 3: Функции и массивы. Функция showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = users
+    .filter(({age: userAge}) => userAge <= age)
+    .map(({name, balance}, index, arr) => {
+      if (index == arr.length - 1) return name + ', ' + balance;
+      return name + ', ' + balance + '\n';
+    })
+  return result.join('');
 }
