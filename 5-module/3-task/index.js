75256f9a271f30bbbb553241b99dd1bb5a82ab21
Batozhab <gorod3012@gmail.com>
2021-06-11 23:03:35 +0800

    Занятие 5: Обработчики событий. Карусель.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const allSlides = document.querySelector('.carousel__inner');
+  const slide = document.querySelector('.carousel__slide');
+
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+
+  arrowLeft.style.display = 'none';
+
+  const slideWidth = slide.offsetWidth;
+  const slideCount = 4;
+  let currentOffset = 0;
+
+  arrowRight.addEventListener('click', (event) => {
+    arrowLeft.style.display = '';
+
+    currentOffset += slideWidth;
+
+    if (currentOffset === slideWidth * (slideCount - 1)) 
+      arrowRight.style.display = 'none';
+    
+    allSlides.style.transform = `translateX(-${currentOffset}px)`;
+  });
+
+  arrowLeft.addEventListener('click', (event) => {
+    arrowRight.style.display = '';
+
+    currentOffset -= slideWidth;
+    
+    if (currentOffset === 0){
+      arrowLeft.style.display = 'none';
+    }
+
+    allSlides.style.transform = `translateX(-${currentOffset}px)`;
+  });
 }
